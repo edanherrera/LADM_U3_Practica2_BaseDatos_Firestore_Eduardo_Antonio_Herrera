ed4bebd875f2ccabd14802da5aea156e483d8df3
Eduardo <edanherreraga@ittepic.edu.mx>
2022-05-07 13:57:55 -0600

    Proyecto base

---
+++ app/src/main/java/mx/tecnm/tepic/ladm_u3_practica2_basedatos_firestore_eduardo_herrera_garcia/ui/home/HomeViewModel.kt
@@ -0,0 +1,13 @@
+package mx.tecnm.tepic.ladm_u3_practica2_basedatos_firestore_eduardo_herrera_garcia.ui.home
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class HomeViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is home Fragment"
+    }
+    val text: LiveData<String> = _text
+}
---
+++ app/src/main/java/mx/tecnm/tepic/ladm_u3_practica2_basedatos_firestore_eduardo_herrera_garcia/ui/notifications/NotificationsViewModel.kt
@@ -0,0 +1,13 @@
+package mx.tecnm.tepic.ladm_u3_practica2_basedatos_firestore_eduardo_herrera_garcia.ui.notifications
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class NotificationsViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is notifications Fragment"
+    }
+    val text: LiveData<String> = _text
+}
